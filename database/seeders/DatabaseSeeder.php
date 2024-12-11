dde899a6457e3153d0dfc486a14fc4aa578f88f4
Camilo <camilomalaver97@gmail.com>
2024-12-12 00:50:26 +0100

    Database seeder base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::table('roles')->insert([
+            ['name' => 'Administrador'],
+            ['name' => 'Facturador'],
+            ['name' => 'Doctor']
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('document_types')->insert([
+            ['name' => 'Cedula de ciudadania', 'abreviature' => 'CC'],
+            ['name' => 'Tarjeta de identidad', 'abreviature' => 'TI'],
+            ['name' => 'Registro civil', 'abreviature' => 'RC'],
+            ['name' => 'Numero de identificacion tributaria', 'abreviature' => 'NIT'],
+        ]);
+
+        DB::table('users')->insert([
+            'id_document_type' => 1,
+            'document_number' => 10101112,
+            'name' => 'Jhon',
+            'last_name' => 'Doe',
+            'email' => 'jhondoe@nomail.com',
+            'password' => bcrypt('password'), // Hash the password
+            'remember_token' => Str::random(10),
+            'id_rol' => 1,
         ]);
     }
 }
