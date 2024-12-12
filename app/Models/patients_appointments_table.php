33093b7be7be575065b452db126bcd649d95153f
DantesEdmundo <jhon06ag@gmail.com>
2024-12-11 20:56:10 -0500

    updating models

---
+++ app/Models/patients_appointments_table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class patients_appointments_table extends Model
+{
+    //
+    protected $table = "patients_appointments_table";
+}
